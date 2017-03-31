71a36dc47f2cab2f77879aa587a39519f4eba95d
ice1000 <ice1000kotlin@foxmail.com>
2017-03-31 15:21:11 +0800

    update

---
+++ 3-31/oral.rb
@@ -0,0 +1,111 @@
+#
+#
+# eye
+# far sighted
+#
+# built
+athlete = :运动员
+#
+# go cycling 骑自行车
+# go biking  骑自行车
+#
+# running nose 流鼻涕 （have a running nose）
+# stuffed nose 鼻子堵
+#
+# blow my nose 擦鼻涕
+# painful
+sore = :酸疼, :酸哑
+#
+# contact lens 接触性眼镜（隐形眼镜）
+#
+# a (slight/serious) cough (轻微/严重)的咳嗽
+# take a temperature 测体温
+#
+
+stomachache = :胃疼
+backache = :背疼
+earache = :耳朵
+
+hurt = [
+		'vi. 疼',
+		'vt. 受伤了',
+]
+
+digestion = '消化'
+indigestion = '不消化'
+
+#
+# in'somnia 失眠
+# I have a insomnia == I couldn't sleep
+#
+
+
+# snore 打呼噜
+
+{:hit => 'the sack'} ## 碰到枕头
+
+fracture = '骨折'
+
+# I.feel 'seek'
+# 我觉得恶心
+
+awesome = :极好
+awful = :极差
+
+miserable = :悲伤的
+#
+# pill 药片 [C.]
+# cold pills 感冒药
+# sleeping pills 安眠药
+#
+# heating pad 暖宝宝
+#
+
+flu = :流感
+
+infect = :传染
+
+# infectious 传染的 ~ disease
+# infection  传染病
+
+drop = :滴的
+# a drop of water 一滴水
+
+syrup = :糖浆
+
+vitamin = /vaitemin/
+
+blister = :溃疡
+sour = :酸的
+
+# If I were you, I'd ...
+
+def apply sth
+	:涂擦
+end
+
+apply 'a doctor'
+
+make 'an appointment'
+
+# Muscle Cream 肌肉膏（防止肌肉酸疼）
+
+# an'tacid 抗酸剂
+
+# side effect
+
+# a stick of
+
+# a tube of
+
+# a box of
+
+# forever young
+
+# a jar of
+
+# OTC = over the counter 非处方
+
+prescription = :药方
+
+# prescribed medicine 处方药
