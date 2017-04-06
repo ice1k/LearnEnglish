014c7fdaa9f3dd6ce24d19eb9f0ba2bb05cc3320
ice1000 <ice1000kotlin@foxmail.com>
2017-04-06 13:44:03 +0800

    www

---
+++ 4-06/notes.md
@@ -0,0 +1,29 @@
+
+
+
+
++ flip through 翻阅
+
++ Don't judge a book by it's cover. 不要以貌取人
+
++ use up 用尽
+
++ spend time on {n./doing} 花时间做...
+
++ become aware of 变得意识到
+
++ In close cooperation with 和...紧密合作
+
++ 近一段时间发生的事情用现在进行时
+
++ die {of/from} 死于
+
++ substance 物质
+
++ be the basis of 是...的基础
+
++ therapy [C.] 治疗
+
++ venom 毒液
+
++ 
---
+++ 4-06/words.rb
@@ -0,0 +1,28 @@
+require_relative '../words'
+
+
+deep_dark_fantasy = {
+		'amateur' => '业余的',
+		'competitive' => '竞争的，竞争激烈的',
+		'draw' => '平局',
+		'goggle' => '护目镜',
+		'hockey' => '曲棍球',
+		'spectator' => '（现场的）观众',
+		'win-win' => '双赢的',
+		'verbal' => '言语的，口头的',
+		'profound' => '深远的（影响）',
+		'subtle' => '细微的', # /satle/
+		'superior' => '优越的',
+		'superiority' => '优越感',
+		'knowledgeable' => '渊博的',
+		'wisdom' => '智慧',
+		'acquire' => '获得',
+		'venom' => '毒液',
+		'shaman' => '萨满',
+}
+
+_4_6 = Words.new
+_4_6.words deep_dark_fantasy
+
+_4_6.review
+# _4_6.listen_write
