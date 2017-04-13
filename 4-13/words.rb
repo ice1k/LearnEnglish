150811b267411d91768b1644782a25cb7947f4f6
ice1000 <ice1000kotlin@foxmail.com>
2017-04-13 10:30:12 +0800

    add

---
+++ 4-13/words.rb
@@ -0,0 +1,13 @@
+require_relative '../words'
+
+my_words = {
+		'venue' => '演出地点，会场',
+		'back up' => '支持',
+		'go up' => '兴建，价格上升',
+		'send off' => '罚下场',
+		'canteen' => '餐厅',
+		'label' => '贴标签（抽象的，具体的）',
+}
+
+_4_13 = Words.new
+_4_13.words my_words
