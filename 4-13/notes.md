40883d30b3ba004dcdc32f6640e613de7abda82f
ice1000 <ice1000kotlin@foxmail.com>
2017-04-13 11:09:25 +0800

    update

--- 4-13/notes.md
+++ 4-13/notes.md
@@ -2,3 +2,18 @@
 
 + benefit from 从...中获益
 
++ create misunderstandings \[C.\] 造成误会
+
++ get some of ... 民白一些
+
++ cultural shock 文化冲突
+
++ extinct 形容词 (die out)
+
++ release a single 发布单曲
+
++ a host of 大量的
+
++ relief 救济金
+
++ be joined by 由...组成 === be consist of
--- build_all.rb
+++ build_all.rb
@@ -3,7 +3,7 @@
 		file_name = "#{month}-#{day}"
 		# noinspection RubyResolve
 		if File.exist? file_name
-			load "#{file_name}/words.rb"
+			load "#{file_name}/words.rb", true
 			print 'Words file for ', file_name, ' is loaded.', "\n"
 		end
 	end
@@ -11,12 +11,18 @@ end
 
 print 'All files loaded.', "\n"
 
+def quit
+	exit 0
+end
+
 if $*[0] != '--no-eval'
 	loop do
 		begin
 			p eval gets
 		rescue SyntaxError
 			print 'syntax error', "\n"
+		rescue NameError
+			print 'name error', "\n"
 		ensure
 		end
 	end
