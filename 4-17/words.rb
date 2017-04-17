03b55d3e896dc8969cad6762e7a9cf3ff2095cbd
ice1000 <ice1000kotlin@foxmail.com>
2017-04-17 10:18:05 +0800

    OMG

---
+++ 4-17/words.rb
@@ -0,0 +1,13 @@
+require_relative '../words'
+
+ws = {
+		'perception' => '认知',
+		'obstacle' => '障碍',
+		'undermine' => '破坏',
+		'numerous' => '许多的，很多的',
+}
+
+_4_17 = Words.new
+_4_17.words ws
+
+_4_17.review
