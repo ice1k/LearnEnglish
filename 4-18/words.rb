72b04f7a77dfa3cde8448dbe71b74236d0144005
ice1000 <ice1000kotlin@foxmail.com>
2017-04-18 17:59:34 +0800

    why why why why why

--- 4-18/words.rb
+++ 4-18/words.rb
@@ -1,16 +1,27 @@
 require_relative '../words'
 
 words_list = {
-  'prace' => '表扬',
-  'hang' => '绞死',
-  'dismiss' => '炒鱿鱼',
-  'negotiation' => '谈判',
-  'contrary' => '相反的',
-  'contrarily' => '相反的',
-  'contradiction' => '矛盾',
+		'prace' => '表扬',
+		'hang' => '绞死',
+		'dismiss' => '炒鱿鱼',
+		'negotiation' => '谈判',
+		'contrary' => '相反的',
+		'contrarily' => '相反的',
+		'contradiction' => '矛盾',
+		'choosy' => '挑剔的',
+		'disposition' => '性情，性格',
+		'in no mood to' => '没有做...的心思和兴致',
+		'compulsive' => '难以抑制的',
+		'provocative' => '引起愤怒的',
+		'reluctant' => '不情愿的，勉强的',
+		'beware' => '当心',
+		'fiddle' => '胡乱摆弄',
+		'suspend' => '使暂停',
+		'twirl' => '旋转',
+		'severe' => '严峻的',
 }
 
 _4_18 = Words.new
 _4_18.words words_list
 
-_4_18.review
+# _4_18.review
