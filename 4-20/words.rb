1660a37b4bfbc624bf6b05b046cd3c4ce070417f
ice1000 <ice1000kotlin@foxmail.com>
2017-04-20 15:03:49 +0800

    boy next door

---
+++ 4-20/words.rb
@@ -0,0 +1,12 @@
+require_relative '../words'
+
+date_a_live = {
+		'expense' => '费用，花销',
+		'outcome' => '结果',
+		'consumption' => '消费(n.)',
+}
+
+_4_20 = Words.new
+_4_20.words date_a_live
+
+# _4_20.review
