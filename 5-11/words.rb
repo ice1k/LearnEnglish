c19e23f92e666938d030d4b113a74130f923d2d8
ice1000 <ice1000kotlin@foxmail.com>
2017-05-11 17:00:59 +0000

    update

--- 5-11/words.rb
+++ 5-11/words.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 require_relative '../words'
 
-take_it_easy {
+take_it_easy = {
 		'loosen up' => '放松',
 		'entitle ... to do' => '授予某人做...的权利',
 		'enpower ... to do' => '给予某人做...的权利',
@@ -10,6 +10,29 @@ take_it_easy {
 		'tense' => '紧张的',
 		'steep' => '陡峭的',
 		'slope' => '坡，倾斜',
+    'urban' => '城市的',
+    'overpainting' => '',
+    'behavioral' => '',
+    'contributor' => '',
+    'carbonniferous' => '',
+    'plantation' => '',
+    'transplanted' => '',
+    'laborious' => '',
+    'exposition' => '',
+    'composer' => '创作者',
+    'galcier' => '',
+    'diverse' => '',
+    'urbanization' => '',
+    'construction' => '',
+    'acorn' => '',
+    'democratic' => '',
+    'stylistic' => '',
+    'projection' => '',
+    'cetaceans' => '',
+    'tension' => '',
+    'ritual' => '',
+    'scholar' => '',
+    'fungi' => '',
 }
 
 _5_11 = Words.new
