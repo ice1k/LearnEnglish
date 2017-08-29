48f887acbe2fd8ca5c7c5543abb387fb0f637a31
ice1000 <ice1000kotlin@foxmail.com>
2017-08-29 18:11:11 +0800

    update

---
+++ 8-29/words.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+require_relative '../words'
+
+_8_29 = Words.new
+
+gg = {
+  'inflate' => '吹嘘',
+  'magnitude' => '等级 强度',
+  'pin down' => '确定 定下 履行',
+  'merely' => '仅仅是',
+  'misgiving' => '疑虑 顾虑',
+  'publication' => '出版物 公开 发行',
+  'proceeding' => '诉讼',
+  'supple' => '灵活的 柔韧的',
+  'suppleness' => '灵活 柔韧',
+  'empirical' => '以经验为依据的',
+  'conjecture' => '猜想 计划',
+  'conjectural' => '猜想的',
+  'compelling' => 'surprising',
+  'impervious' => '不可渗透 不受影响的',
+  'vigorous' => '有生命力的',
+}
+
+_8_29.words gg
+
